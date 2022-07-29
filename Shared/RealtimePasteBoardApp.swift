f04561e44d7d15ebd7f45d3a760f64dddbe8999f
Book.s <107929412+SuepnBook@users.noreply.github.com>
2022-07-29 14:45:44 +0800

    Initial Commit

---
+++ Shared/RealtimePasteBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealtimePasteBoardApp.swift
+//  Shared
+//
+//  Created by Book on 2022/7/29.
+//
+
+import SwiftUI
+
+@main
+struct RealtimePasteBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
