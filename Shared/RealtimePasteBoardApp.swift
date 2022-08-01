e31e1f65dd011fdc351c8bdb98eec59a484e9237
Book.s <107929412+SuepnBook@users.noreply.github.com>
2022-08-02 02:28:09 +0800

    feat: setup env

--- Shared/RealtimePasteBoardApp.swift
+++ Shared/RealtimePasteBoardApp.swift
@@ -6,12 +6,32 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        FirebaseApp.configure()
+        RealtimeDatabase.shared.setupObserve()
+        return true
+    }
+}
 
 @main
 struct RealtimePasteBoardApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            NavigationView {
+            PasteBoardListView()
+//            }
         }
     }
 }
